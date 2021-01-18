e861949dd3861f5fdd852ea74033f25ff700f91a
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-01-18 20:36:45 +0545

    modified detail and index html

--- soros/settings.py
+++ soros/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'b&w-wlsd_&)969al74hxfd=q=s!s&v+u@u9cl9t16)rgq-vvn&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['soros.pythonanywhere.com']
+ALLOWED_HOSTS = ['soros.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
